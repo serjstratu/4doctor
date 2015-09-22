f468bf855fcae8db17e8daa43b70d3769a9901b6
serjstratu <serghei.stratu@orange.md>
2015-09-22 14:51:00 +0300

    cart html changes

--- js/custom.js
+++ js/custom.js
@@ -12,3 +12,15 @@ $(function(){
         }
     });
 });
+
+function modify_qty(val) {
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
+
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+
+    document.getElementById('qty').value = new_qty;
+    return new_qty;
+}
